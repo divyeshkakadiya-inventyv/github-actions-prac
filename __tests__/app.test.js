5e5d550e1aab48e2e8f7d1f8334c6b048e71159b
divyesh kakadiya <divyesh.kakadiya@inventyv.com>
2024-12-02 15:05:13 +0530

    Update app.js and add test for 5 + 3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,3 +5,8 @@ test('adds 1 + 2 to equal 3', () => {
     console.log(add(1, 2));
   expect(add(1, 2)).toBe(3);
 });
+
+// New test case
+test('adds 5 + 3 to equal 8', () => {
+  expect(add(5, 3)).toBe(8);
+});
